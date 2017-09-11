cd42a6043812cdb45678c19240fa5da97d2db6dc
zhailiang <zhailiang@haoduocheyou.com>
2017-09-12 01:43:05 +0800

    update

--- imooc-security-app/src/main/java/com/imooc/security/app/ImoocAuthorizationServerConfig.java
+++ imooc-security-app/src/main/java/com/imooc/security/app/ImoocAuthorizationServerConfig.java
@@ -3,8 +3,20 @@
  */
 package com.imooc.security.app;
 
+import org.apache.commons.lang.ArrayUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.builders.InMemoryClientDetailsServiceBuilder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import com.imooc.security.core.properties.OAuth2ClientProperties;
+import com.imooc.security.core.properties.SecurityProperties;
 
 /**
  * @author zhailiang
@@ -12,6 +24,38 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @Configuration
 @EnableAuthorizationServer
-public class ImoocAuthorizationServerConfig {
+public class ImoocAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private SecurityProperties securityProperties;
+	
+	@Autowired
+	private TokenStore tokenStore;
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints
+			.tokenStore(tokenStore)
+			.authenticationManager(authenticationManager)
+			.userDetailsService(userDetailsService);
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		InMemoryClientDetailsServiceBuilder builder = clients.inMemory();
+		if (ArrayUtils.isNotEmpty(securityProperties.getOauth2().getClients())) {
+			for (OAuth2ClientProperties config : securityProperties.getOauth2().getClients()) {
+				builder.withClient(config.getClientId()).secret(config.getClientSecret())
+						.accessTokenValiditySeconds(config.getAccessTokenValiditySeconds())
+						.authorizedGrantTypes("refresh_token", "password").scopes("all", "read", "write");
+			}
+		}
+	}
 
 }
